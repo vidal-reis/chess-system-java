380e600a7bf86aeb1744a5bc08cc35e5578819c8
Gustavo Vidal <reis.vidal21@gmail.com>
2020-02-18 16:02:39 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessExecption.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessExecption extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExecption(String msg) {
+		super(msg);
+	}
+
+}
