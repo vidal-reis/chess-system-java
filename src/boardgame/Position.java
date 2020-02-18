24fc2c20ed3213e1679d591fbe51f24c732c298c
Gustavo Vidal <reis.vidal21@gmail.com>
2020-02-18 11:35:31 -0300

    First Class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position() {
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColun() {
+		return column;
+	}
+
+	public void setColun(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return row + " , " + column;
+	}
+}
